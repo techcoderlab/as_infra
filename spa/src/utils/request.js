21910efd5f498f61858a5a868a6a0212cd42c629
techcoderlab <c.hassanshahzad@gmail.com>
2026-08-16 17:56:55 +0500

    Fix: SPA 1

--- spa/src/utils/request.js
+++ spa/src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: '/api'
 })
 
 axios.defaults.withCredentials = true
